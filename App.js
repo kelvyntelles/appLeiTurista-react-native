fdeac18c18da01bcfe5f5fb0b3ebd3dca49e760f
Kelvyn Telles <kelvyn.telles@gmail.com>
2022-03-31 16:58:30 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,62 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [matricula, setMatricula] = useState('');
+  const [codigo, setCodigo] = useState('');
+  const [situacao, setSituacao] = useState('');
+
+  let dados = []
+
+  const botao = () => {
+    if(matricula == '' || codigo == '' || situacao == '') {
+      alert('Dados invalidos!')
+    } else {
+      dados.push(
+        {
+          "matricula": matricula,
+          "codigo": codigo,
+          "situacao": situacao,
+        }
+      )
+      alert(
+        'Dados salvos!'
+      )
+      setMatricula('')
+      setCodigo('')
+      setSituacao('')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Informe sua matrícula:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setMatricula}
+        value={matricula}
+      />
+
+      <Text style={styles.text}>Informe o código:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setCodigo}
+        value={codigo}
+      />
+
+      <Text style={styles.text}>Informe a situação:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setSituacao}
+        value={situacao}
+      />
+
+      <Button
+        title="Salvar"
+        onPress={botao}
+      />
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +68,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+  },  
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
